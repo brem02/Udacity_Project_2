c8118b44281c7e2841ebf06e19efdd43be0583db
brem02 <122722304+brem02@users.noreply.github.com>
2023-05-27 15:08:26 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,12 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 3.0)
+
+    @task(1)
+    def test1(self):
+        self.client.get("http://localhost:5000")
+
+    @task(2)
+    def test2(self):
+        self.client.post("http://localhost:5000/predict")
